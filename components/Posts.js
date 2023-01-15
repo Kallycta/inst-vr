5b6262d224465bae3f4101e0617ffca18666ac0d
Your Name <e@w.com>
2023-01-15 17:35:26 +0300

    deploy_to_Vercel

---
+++ components/Posts.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, [db]); 
+  return (
+    <div>
+      {posts.map((post, i) => (
+        <Post
+          key={i}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
+      ))}
+      
+    </div>
+  );
+}
